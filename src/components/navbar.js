41c732b3e7bce67f8797d9b4cfc3ca3b746112bf
ezarnafis <ezarnafis01@gmail.com>
2020-08-26 13:28:22 +0700

    Add font awesome, update navbar style

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default (props) => {
   return (
-    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-white">
-      <Link to="/" className="navbar-brand">React Ku</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/product" className="nav-link">Product</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/form" className="nav-link">Form</Link>
-          </li>
-        </ul>
-        <form className="input-group form-search w-70 mx-auto">
-          <input type="text" className="form-control" placeholder="Cari sepatu nike"/>
-          <div className="input-group-append">
-            <button className="btn btn-search" type="button">Button</button>
-          </div>
-        </form>
-        <Link to="/login"><button className="btn btn-outline-primary btn-login btn-header mr-2">Masuk</button></Link>
-        <Link to="/register"><button className="btn btn-primary btn-header">Daftar</button></Link>
+    <div className="header-wrapper sticky-top bg-white">
+      <div className="top-header">
+        <div className="left-top-header">
+          <FontAwesomeIcon icon={faMobileAlt} className="fa-lg" />
+          <Link to="/mobile-apps/" target="_blank" className="header-link">
+            Download ReactKu App
+          </Link>
+        </div>
+        <div className="right-top-header">
+          <Link to="/about/" target="_blank" className="header-link">Tentang ReactKu</Link>
+          <Link to="/promo/" target="_blank" className="header-link">Promo</Link>
+          <Link to="/help/" target="_blank" className="header-link">ReactKu Care</Link>
+        </div>
       </div>
-    </nav>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <Link to="/" className="navbar-brand">React Ku</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/product" className="nav-link">Product</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/form" className="nav-link">Form</Link>
+            </li>
+          </ul>
+          <form className="input-group form-search w-70 mx-lg-auto mx-sm-3">
+            <input type="text" className="form-control" placeholder="Cari sepatu nike"/>
+            <div className="input-group-append">
+              <button className="btn btn-search" type="button">
+                <FontAwesomeIcon icon={faSearch} />
+              </button>
+            </div>
+          </form>
+          <Link to="/login"><button className="btn btn-outline-primary btn-login btn-header mr-2">Masuk</button></Link>
+          <Link to="/register"><button className="btn btn-primary btn-header">Daftar</button></Link>
+        </div>
+      </nav>
+    </div>
   )
 }
